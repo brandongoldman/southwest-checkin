9e19185ce00095bf4d2eb3c0735dc5bcf41e2069
Brandon Goldman <brandon.m.goldman@gmail.com>
2017-12-23 21:14:48 -0500

    Create checkin.js

---
+++ checkin.js
@@ -0,0 +1,31 @@
+/* Instructions: ** Works in Google Chrome **
+	1. Open Southwest Check-In page: https://www.southwest.com/air/check-in/
+	2. Type in Confirmation Number and Name Information
+	3. Open up the Developer Console (View > Developer > JavaScript Console)
+	4. Copy and past this code in the console (change the time) and press Enter.
+
+	Code will click the submit button at exact time you enter, down to the second!
+*/
+
+// ADD YOUR FLIGHT TIME HERE (Example: "7:30:00 PM")
+var flightTime = "9:09:40 PM";
+
+// Find button on page you need to click to submit
+var checkIn = document.getElementById("form-mixin--submit-button");
+
+// Determine Time of Day, and perform action at flightTime
+var today = new Date();
+var flightDate = (today.getMonth()+1) +'/'+ today.getDate() +'/'+ today.getFullYear();
+var flightTimeMil = new Date( flightDate + ' ' + flightTime ).getTime();
+
+timeMil = new Date().getTime();					// gets current time
+countDownMil = flightTimeMil - timeMil;	// finds difference in time between current time and flight time
+
+// Set Timer
+function checkedIn () {
+	checkIn.click();
+	console.log ('Successfully checked in.');
+};
+
+// Driver Function
+setTimeout(checkedIn, countDownMil);
